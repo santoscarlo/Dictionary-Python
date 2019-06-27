6afee95ca41c1675c83a74219328a60c00adbefc
Carlo Santos <santoscarlo111@gmail.com>
2019-06-27 07:04:22 -0700

    MonthConversion commit

---
+++ lesson.py
@@ -0,0 +1,20 @@
+
+key = input("Enter key: ")
+monthConversions = {
+    "Jan": "January",
+    "Feb": "February",
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": "August",
+    "Sep": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dec": "December",
+    
+    
+
+    }
+print(monthConversions.get(key))
